8fc0b2ca97c1984366a3fc4a1e5e1369147e9284
Ravneel Lal <rav.lal@unifysolutions.net>
2018-07-27 08:42:15 +1200

    Add project files.

---
+++ StockX/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.MainAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private async Task MainAsync()
+        {
+            HttpClient client = new HttpClient();
+            
+            
+
+            //client.PostAsync("https://stockx.com/api/login");
+        }
+        }
+}
